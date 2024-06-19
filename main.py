56a36bb4ec28be0988ae4beb5c4b45e397b3eb1b
Kingcode <136673119+o2real@users.noreply.github.com>
2024-06-19 15:13:12 +0900

    gogo

---
+++ main.py
@@ -0,0 +1,15 @@
+# import another_module
+#
+# print(another_module.another_variable)
+
+from turtle import Turtle, Screen
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("orange")
+timmy.forward(300)
+timmy.
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
