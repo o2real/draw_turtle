bcfe9011dc960b62235991f02d9ef61a855d09f8
Kingcode <136673119+o2real@users.noreply.github.com>
2024-06-21 01:06:17 +0900

    prettytable...

--- main.py
+++ main.py
@@ -2,13 +2,24 @@
 #
 # print(another_module.another_variable)
 
-from turtle import Turtle, Screen
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("orange")
-timmy.forward(300)
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("orange")
+# timmy.forward(300)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+from prettytable import PrettyTable
+table = PrettyTable()
+
+
+table.field_names = ["poketmon name", "type"]
+table.add_row(["pikachu", "electric"])
+table.add_row(["squrtle", "water"])
+table.add_row(["charmander", "fire"])
+
+print(table)
